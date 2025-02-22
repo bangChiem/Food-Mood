2b3fcc6c81d3353293921aa943c2ad32dbc9d222
baoasm1 <baoasm1@tcnj.edu>
2025-02-22 15:56:43 -0500

    testing

--- front-end/src/pages/CardPage.jsx
+++ front-end/src/pages/CardPage.jsx
@@ -8,7 +8,7 @@ export default function CardPage(){
             gap: "20px",
             padding: "20px"
           }}>
-            <Card title={"Card1"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quaerat harum perspiciatis doloribus aspernatur optio autem, distinctio quos, excepturi a blanditiis accusamus molestias vero! Quisquam quidem explicabo impedit dolore sed"} image={"/lebron.png"}></Card>
+            <Card title={"Card500"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quaerat harum perspiciatis doloribus aspernatur optio autem, distinctio quos, excepturi a blanditiis accusamus molestias vero! Quisquam quidem explicabo impedit dolore sed"} image={"/lebron.png"}></Card>
             <Card title={"Card2"}></Card>
             <Card title={"Card3"}></Card>
             <Card title={"Card4"}></Card>
